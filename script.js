003f8de136df9840530b1b96ff4e6f444d03fbe0
Gerry1402 <gerard.vello@gmail.com>
2024-06-13 12:01:25 +0200

    Index, script y style creados

---
+++ script.js
@@ -0,0 +1,23 @@
+let numbers = document.querySelectorAll("[countTo]");
+
+numbers.forEach((number) => {
+    let ID = number.getAttribute("id");
+    let value = number.getAttribute("countTo");
+    let countUp = new CountUp(ID, value);
+
+    if (number.hasAttribute("data-decimal")) {
+    const options = {
+          decimalPlaces: 1,
+        };
+    countUp = new CountUp(ID, 2.8, options);
+    } else {
+    countUp = new CountUp(ID, value);
+    }
+
+    if (!countUp.error) {
+    countUp.start();
+    } else {
+    console.error(countUp.error);
+    number.innerHTML = value;
+    }
+}); 
