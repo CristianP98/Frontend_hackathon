6f93f0bb7a2b24f66ec93e115e8a9eaf7c5c52d1
Gerry1402 <gerard.vello@gmail.com>
2024-06-13 12:19:50 +0200

    menu y stats anadidos

--- JS/script.js
+++ JS/script.js
@@ -20,4 +20,11 @@ numbers.forEach((number) => {
     console.error(countUp.error);
     number.innerHTML = value;
     }
-}); 
+});
+
+const menuToggle = document.getElementById('menu-toggle');
+const mobileMenu = document.getElementById('mobile-menu');
+
+menuToggle.addEventListener('click', function () {
+  mobileMenu.classList.toggle('hidden');
+});
